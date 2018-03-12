eff794972766f7dbb994935fe0f4919291d9b9a3
Magdalena <magi.beljan@gmail.com>
2018-03-12 22:05:32 +0100

    menu screen

---
+++ SpyFall/app/src/menuScreen.jsx
@@ -0,0 +1,44 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./button.jsx";
+
+export default class MenuScreen extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+	}
+
+	createNewGame()
+	{
+		this.props.create();
+	}
+
+	enterExistingGame()
+	{
+		this.props.enter();
+	}
+
+	rulesOfTheGame()
+	{
+		this.props.rules();
+	}
+
+	listOfLocations()
+	{
+		this.props.locations();
+	}
+
+	render()
+	{
+		return (
+			<div>
+				<h1>Dobrodošli u SPYFALL!</h1>
+				<Button title="Kreiraj igru" onClick={this.createNewGame.bind(this)} />
+				<Button title="Uključi se u igru" onClick={this.enterExistingGame.bind(this)} />
+				<Button title="Pravila igre" onClick={this.rulesOfTheGame.bind(this)} />
+				<Button title="Lokacije" onClick={this.listOfLocations.bind(this)} />
+			</div>
+		);
+	}
+}
