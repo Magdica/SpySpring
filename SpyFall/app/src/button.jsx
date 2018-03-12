8f0d05e86715ce6f95f9cd9f9aa5965be2ac2e9e
Magdalena <magi.beljan@gmail.com>
2018-03-12 22:05:20 +0100

    button component

---
+++ SpyFall/app/src/button.jsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Button extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+	}
+
+	onClick()
+	{
+		this.props.onClick();
+	}
+
+	render()
+	{
+		return <div id="button" onClick={this.onClick.bind(this)}>{this.props.title}</div>;
+	}
+};
