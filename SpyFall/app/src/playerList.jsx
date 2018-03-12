d31fc68095b7f9f42f577b86f3d22de14fdf3e00
Magdalena <magi.beljan@gmail.com>
2018-03-12 22:06:21 +0100

    unfinished player list

---
+++ SpyFall/app/src/playerList.jsx
@@ -0,0 +1,18 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+export default class PlayerList extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+	}
+
+	render()
+	{
+		const list = this.props.players.map((player) => <li>{player}</li>);
+		return (
+			<ul>{list}</ul>
+		);
+	}
+}
