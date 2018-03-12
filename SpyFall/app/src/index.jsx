635640c01b33b92f444868d712f5b0b2c9dd27a5
Magdalena <magi.beljan@gmail.com>
2018-03-12 22:07:15 +0100

    csproj, index jsx and less files

--- SpyFall/app/src/index.jsx
+++ SpyFall/app/src/index.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./button.jsx";
+import PlayerList from "./playerList.jsx";
+import RulesScreen from "./rulesScreen.jsx";
+import CreateGameScreen from "./createGameScreen.jsx";
+import MenuScreen from "./menuScreen.jsx";
 
-var element = (
-    <div>
-        <h1> good afternoon! </h1>
-        <h2> bye bye bye </h2>
-    </div>
-);
+class Application extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+			toBeRendered: <MenuScreen rules={this.onRulesClicked.bind(this)}/> };
+	}
 
-ReactDOM.render(element, document.getElementById("app"));
+
+
+	onButtonClick()
+	{
+		alert("bu bu");
+	}
+
+	onRulesClicked()
+	{
+		this.setState({ toBeRendered: <RulesScreen /> });
+	}
+
+	render()
+	{
+		return (
+			<div>
+				{this.state.toBeRendered}
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Application/>, document.getElementById("app"));
